5291507dc20aa3696c472f230ce7983524a258a8
masyuko0222 <masyuko0222@gmail.com>
2024-10-02 22:52:18 +0900

    ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
 
   get '/login', to: 'sessions#new'
   get '/auth/:provider/callback', to: 'sessions#callback'
-  get '/auth/failure', to: 'sessions#failure'
+  get '/auth/failure', to: redirect('/login')
+
+  get '/reading_clubs', to: 'reading_clubs#index'
 end
