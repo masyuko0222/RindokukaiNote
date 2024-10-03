f8e44383e20bfefa91aaaf0752d92346e9c1910e
masyuko0222 <masyuko0222@gmail.com>
2024-10-03 20:47:07 +0900

    UserとReadingClubの多対多用の中間テーブルのモデル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_10_01_115542) do
+ActiveRecord::Schema[7.1].define(version: 2024_10_03_111912) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "participants", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "reading_club_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["reading_club_id"], name: "index_participants_on_reading_club_id"
+    t.index ["user_id"], name: "index_participants_on_user_id"
+  end
 
   create_table "reading_clubs", force: :cascade do |t|
     t.string "title", null: false
@@ -32,4 +41,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_10_01_115542) do
     t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
   end
 
+  add_foreign_key "participants", "reading_clubs"
+  add_foreign_key "participants", "users"
 end
