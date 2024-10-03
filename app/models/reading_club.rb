c50384a89d64a37ebfb6748ee7345e013debe095
masyuko0222 <masyuko0222@gmail.com>
2024-10-03 20:47:45 +0900

    UserとReadingClubの多対多関連付け

--- app/models/reading_club.rb
+++ app/models/reading_club.rb
@@ -4,6 +4,9 @@ class ReadingClub < ApplicationRecord
   validates :title, presence: true
   validates :finished, inclusion: { in: [true, false] }
 
+  has_many :participants, dependent: :destroy
+  has_many :users, through: :participants
+
   scope :finished, -> { where(finished: true) }
   scope :open, -> { where(finished: false) }
 
