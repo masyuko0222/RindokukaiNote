ebd85bbb6fe4494977fe0f0cb9bedf0f910c91fd
masyuko0222 <masyuko0222@gmail.com>
2024-10-08 23:31:10 +0900

    Userが参加中の輪読会を取得する命名を分かりやすくした

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
   validates :provider, presence: true
 
   has_many :participants, dependent: :destroy
-  has_many :reading_clubs, through: :participants
+  has_many :participating_reading_clubs, through: :participants, source: :reading_club
 
   class << self
     def find_or_create_from_discord_info(discord_info)
